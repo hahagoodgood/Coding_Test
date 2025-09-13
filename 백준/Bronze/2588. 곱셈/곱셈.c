ec6faff979a494459cd103e6da99184d0ae89a29
김동혁 <144420387+hahagoodgood@users.noreply.github.com>
2025-09-13 15:44:04 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	scanf("%d\n%d", &a, &b);
+	printf("%d\n%d\n%d\n%d\n", a * (b % 10), a * (b % 100 - b % 10)/10, a * (b - b % 100)/100, a * b);
+}

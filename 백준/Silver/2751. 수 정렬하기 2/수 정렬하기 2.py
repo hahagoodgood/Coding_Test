81afe9ecc7dbaef794844e37711cbf1ba0875043
김동혁 <144420387+hahagoodgood@users.noreply.github.com>
2025-09-13 10:42:50 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4056 ms, Memory: 99964 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,34 @@
+import sys
+
+def merge_sort2(arr):
+    if len(arr) <= 1:
+        return arr
+    else :
+        mid = len(arr)//2
+        leftArr = merge_sort2(arr[:mid])
+        rightArr = merge_sort2(arr[mid:])
+        return merge(leftArr, rightArr)
+
+def merge(leftArr, rightArr):
+    leftIndex = rightIndex = 0
+    result = []
+    while leftIndex < len(leftArr) and rightIndex < len(rightArr) :
+        if leftArr[leftIndex] < rightArr[rightIndex]:
+            result.append(leftArr[leftIndex])
+            leftIndex += 1
+        else:
+            result.append(rightArr[rightIndex])
+            rightIndex += 1
+    result.extend(leftArr[leftIndex:])
+    result.extend(rightArr[rightIndex:])
+    return result
+
+
+num = int(sys.stdin.readline().rstrip())
+numlist = [int(sys.stdin.readline().rstrip()) for i in range(num)]
+
+# 적용하고 싶은 함수를 적용하여 해결하시오! 
+sorted_list = merge_sort2(numlist) 
+
+for item in sorted_list:
+    sys.stdout.write(str(item) + '\n')

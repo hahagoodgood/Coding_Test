01d7ef1f4370c9339799a430f9fa18bd63305606
김동혁 <144420387+hahagoodgood@users.noreply.github.com>
2026-01-22 22:48:57 +0900

    [Bronze V] Title: 문자열, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -0,0 +1,6 @@
+# Read the number of test cases and iterate
+for _ in range(int(input())):
+    # Read input string
+    i_str = input()
+    # Print the first and last characters
+    print(i_str[0] + i_str[-1])

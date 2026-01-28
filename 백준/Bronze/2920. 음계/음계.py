b8ba304acd4459f29dc0f447aae66179fa215bee
김동혁 <144420387+hahagoodgood@users.noreply.github.com>
2026-01-28 13:27:06 +0900

    [Bronze II] Title: 음계, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+sing = input()
+ascending = "1 2 3 4 5 6 7 8"
+descending = "8 7 6 5 4 3 2 1"
+if sing == ascending :
+    print("ascending")
+elif sing == descending :
+    print("descending")
+else : print("mixed")

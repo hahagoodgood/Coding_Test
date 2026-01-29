245eed70f8acef5745ec3d1d426fbc2c6645cd2d
김동혁 <144420387+hahagoodgood@users.noreply.github.com>
2026-01-30 00:19:12 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -6,12 +6,11 @@ alphabet_list = [-1] * 26  # Initialize list with -1 for each of 26 alphabets
 # Find the first occurrence position of each alphabet in the input string
 for idx, character in enumerate(S):
     # Calculate alphabet index (a=0, b=1, ..., z=25)
-    alph_location = ord(character) - 97
+    pos = ord(character) - 97
     
     # Only update if this alphabet hasn't appeared before
-    if alphabet_list[alph_location] == -1:
-        alphabet_list[alph_location] = idx
+    if alphabet_list[pos] == -1:
+        alphabet_list[pos] = idx
 
 # Print the first occurrence position of each alphabet
-for item in alphabet_list:
-    sys.stdout.write(str(item) + ' ')
+print(*alphabet_list)

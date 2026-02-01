cdaead0029cf65e351547e60f3d52966fb379aa3
김동혁 <144420387+hahagoodgood@users.noreply.github.com>
2026-02-01 20:29:32 +0900

    [Bronze III] Title: 웰컴 키트, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -2,11 +2,8 @@ N = int(input())
 size = list(map(int, input().split()))
 T, P = map(int, input().split())
 
-# Calculate minimum T-shirt bundles needed
-t_set = 0
-for item in size:
-    # If remainder exists, need one extra bundle; otherwise exact division
-    t_set += (item // T + 1) if item % T else item // T
+# Round-up division: (item + T - 1) // T
+t_set = sum((item + T - 1) // T for item in size)
 
 print(t_set)
-print(str(N // P) + " " + str(N % P))
+print(N // P, N % P)
